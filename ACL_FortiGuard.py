de13f233cccc116b1d67f5cd60fbdbd59835bcdf
Sidharth S <siddhuinchakattu@gmail.com>
2020-08-06 21:43:38 +0530

    v1.0.0

---
+++ ACL_FortiGuard.py
@@ -0,0 +1,73 @@
+'''
+Address Category Listing using FortiGuard Web Filter Lookup
+Author : Sidharth S - @sidharthism - https://github.com/sidharthism
+v1.0.0
+'''
+
+import requests
+import urllib.parse
+from bs4 import BeautifulSoup  
+from datetime import datetime
+import time
+
+now = datetime.now()
+dt_string = now.strftime("%b-%d-%Y-%H-%M-%S")
+cat_file = "categories-"+ dt_string +".csv"
+serviceurl = "https://fortiguard.com/webfilter?"
+args = dict()
+count = 0
+
+try:
+    fh_addr = open("addresses.txt")   
+except:   
+    print("\nCould not open addresses.txt\n")
+    print("Trying to create addresses.txt")
+    try:
+        fh_addr = open("addresses.txt", "w")
+        fh_addr.close()
+        fh_addr = open("addresses.txt")
+    except:
+        print("\nCould not create addresses.txt\n")
+        exit()
+if len(fh_addr.read()) == 0:
+    print("\nAdd addresses to addresses.txt\n")
+    exit()
+else:
+    fh_addr.seek(0)
+try:   
+    fh_cat = open(cat_file,"w")
+except:
+    print("Could not create output file...")
+    exit()    
+
+for address in fh_addr:
+    category = None
+    args['q'] = address.strip()
+    args['version'] = 8
+    url = serviceurl + urllib.parse.urlencode(args)
+    print('Retrieving', args['q'])
+    try:
+        data = requests.get(url)
+        soup = BeautifulSoup(data.text, 'html.parser')
+        try:
+            category = soup.find('h4', {'class': 'info_title'}).get_text()
+        except:
+            category = None
+            print("Category not found!", args['q'])
+        if category is not None:
+            count += 1
+            cat = category[category.index(':')+1:].strip()
+            print(cat)
+            fh_cat.write(str(args['q'] + " , " + cat + "\n"))
+        if count%10 == 0:
+            print("\nContinuing in 5 seconds...\n")
+            time.sleep(5)
+    except KeyboardInterrupt:
+        print("\nKeyboard Interrupt\n")
+        break
+    except:
+        print("Error retrieving",args['q'])
+        
+fh_cat.close()
+fh_addr.close()
+print("Retrieval of",count,"addresses Successful...\nWritten to",cat_file)
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# ACL
 Address Category Listing using FortiGuard Web Filter Lookup
+Author : Sidharth S - @sidharthism - https://github.com/sidharthism/ACL
+v1.0.0
+
+========================== Requirements ===========================
+
+BeautifulSoup^4.0.0
+
+============================= Usage ===============================
+
+Add the addresses to address.txt (The program will automatically create one if the file doesn't exist. You can then add the addresses.)
+
+In a shell/cmd/powershell run >python ACL_FortiGuard.py
